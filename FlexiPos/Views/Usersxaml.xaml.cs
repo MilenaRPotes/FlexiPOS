f9ad05c5d41f44927b2a212fb1315eedb4010eb8
MilenaRPotes <110207483+MilenaRPotes@users.noreply.github.com>
2025-05-16 15:24:43 -0500

    Crud desing, conexion BD

--- FlexiPos/Views/Usersxaml.xaml.cs
+++ FlexiPos/Views/Usersxaml.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +26,26 @@ namespace FlexiPos.Views
         public Usersxaml()
         {
             InitializeComponent();
+            LoadData();
         }
+
+
+
+        SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["conexionDB"].ConnectionString);
+        //Method load data
+        void LoadData() 
+        {
+            //fill the datagrid with the list of users
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("Select IdUser, Names, LastNames, Phone, Email, NamePrivilege from Users inner join privileges on Users.Privilege=privileges.IdPrivilege order by IdUser ASC", connection);
+            SqlDataAdapter dadapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dadapter.Fill(dt);
+            GridData.ItemsSource = dt.DefaultView;
+            connection.Close();
+
+        }
+
 
         private void AddUser(object sender, RoutedEventArgs e)
         {
